ba6a63eb509fcf58635afc367f7c060069a84303
rharris8198 <rh76559p@pace.edu>
2020-11-13 22:11:20 -0500

    Navigation Template
    Added template for navigation

---
+++ ios/Pods/Target Support Files/Pods-Chimera-ChimeraTests/Pods-Chimera-ChimeraTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Chimera_ChimeraTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Chimera_ChimeraTestsVersionString[];
+
